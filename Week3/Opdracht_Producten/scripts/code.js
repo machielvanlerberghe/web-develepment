9f2ba1f327fbc496efd6be12035853c4b9210563
machielvanlerberghe <92442947+machielvanlerberghe@users.noreply.github.com>
2022-03-09 15:18:21 +0100

    a

--- Week3/Opdracht_Producten/scripts/code.js
+++ Week3/Opdracht_Producten/scripts/code.js
@@ -1,21 +1,21 @@
+let aantal =document.getElementsByTagName("input");
+let btw =document.getElementsByClassName("btw");
+let prijzen =document.getElementsByClassName("prijs");
+let subtotaal = document.getElementsByClassName("subtotaal");
+let totaal = document.getElementsByClassName("totaal");
+
 const setup = () => {
-    let aantal =document.getElementsByTagName("input");
-    let btw =document.getElementsByClassName("btw");
-    let prijzen =document.getElementsByClassName("prijs");
     let element =  document.getElementsByTagName("button");
-    element.addEventListener('click', bereken);
+    element.addEventListener('click', Bereken);
 }
-const bereken = () => {
-    let aantal =document.getElementsByTagName("input");
-    let btw =document.getElementsByClassName("btw");
-    let prijzen =document.getElementsByClassName("prijs");
-    let subtotaal = document.getElementsByClassName("prijs");
-    let totaal = document.getElementsByClassName("prijs");
+
+const Bereken = () => {
     for (let i = 0; i < subtotaal.length; i++) {
+        console.log('1');
         subtotaal[i] = (prijzen[i].value * aantal[i].value) * (1 + (btw[i].value / 100));
         totaal[0] = 0;
         totaal[0] += subtotaal[i].value;
-        console.log('1');
     }
 }
+
 window.addEventListener("load", setup);
