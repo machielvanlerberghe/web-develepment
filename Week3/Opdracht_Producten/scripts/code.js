dfb1a15ae978726ad7da4cf6f67f7fd46be1e538
machielvanlerberghe <92442947+machielvanlerberghe@users.noreply.github.com>
2022-03-10 17:14:49 +0100

    a

--- Week3/Opdracht_Producten/scripts/code.js
+++ Week3/Opdracht_Producten/scripts/code.js
@@ -1,21 +1,36 @@
-let aantal =document.getElementsByTagName("input");
-let btw =document.getElementsByClassName("btw");
-let prijzen =document.getElementsByClassName("prijs");
-let subtotaal = document.getElementsByClassName("subtotaal");
-let totaal = document.getElementsByClassName("totaal");
-
 const setup = () => {
-    let element =  document.getElementsByTagName("button");
-    element.addEventListener('click', Bereken);
+
+    let herberekenBtn = document.getElementById("herbereken");
+    herberekenBtn.addEventListener("click", herberekenTotaal)
 }
 
-const Bereken = () => {
-    for (let i = 0; i < subtotaal.length; i++) {
-        console.log('1');
-        subtotaal[i] = (prijzen[i].value * aantal[i].value) * (1 + (btw[i].value / 100));
-        totaal[0] = 0;
-        totaal[0] += subtotaal[i].value;
-    }
+const herberekenTotaal = () => {
+
+    let subtotalen = document.getElementsByClassName("subtotaal");
+    let totaal = document.getElementById("totaal");
+
+    // prijzen
+    let priceProduct1 = parseFloat(document.getElementById("priceProduct1").textContent);
+    let priceProduct2 = parseFloat(document.getElementById("priceProduct2").textContent);
+    let priceProduct3 = parseFloat(document.getElementById("priceProduct3").textContent);
+
+    // aantallen
+    let aantalProduct1 = document.getElementById("product1").value;
+    let aantalProduct2 = document.getElementById("product2").value;
+    let aantalProduct3 = document.getElementById("product3").value;
+
+    let subtotaalProduct1 = (aantalProduct1 * priceProduct1) * 1.06;
+    let subtotaalProduct2 = (aantalProduct2 * priceProduct2) * 1.21;
+    let subtotaalProduct3 = (aantalProduct3 * priceProduct3) * 1.21;
+
+    subtotalen[0].textContent = `${subtotaalProduct1.toFixed(2).toString()} Eur`;
+    subtotalen[1].textContent = `${subtotaalProduct2.toFixed(2).toString()} Eur`;
+    subtotalen[2].textContent = `${subtotaalProduct3.toFixed(2).toString()} Eur`;
+
+    totaal.textContent = (subtotaalProduct1 + subtotaalProduct2 + subtotaalProduct3).toFixed(2).toString();
+
 }
+
+
 
 window.addEventListener("load", setup);
