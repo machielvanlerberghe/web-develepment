d5654d2d6297041d182c1c798a4e05a73692c59d
machielvanlerberghe <92442947+machielvanlerberghe@users.noreply.github.com>
2022-03-31 18:58:52 +0200

    a

--- Week6/opdracht_colorpicker/scripts/code.js
+++ Week6/opdracht_colorpicker/scripts/code.js
@@ -17,8 +17,19 @@ const setup = () => {
 	slider1 = document.getElementById("slider1");
 	slider2 = document.getElementById("slider2");
 	slider3 = document.getElementById("slider3");
+	button = document.getElementById("button")
 	slider1.addEventListener("change", update)
 	slider2.addEventListener("change", update)
 	slider3.addEventListener("change", update)
+
+	let value1 = slider1.value;
+	let value2 = slider2.value;
+	let value3 = slider3.value;
+	button.addEventListener("click", save(value1, value2, value3))
+}
+
+const save = (red,green,blue) => {
+	let save = document.createElement('div')
+	save.style.backgroundColor("rgb(" + red + ", " + green + ", " + blue + ")")
 }
 window.addEventListener("load", setup);
